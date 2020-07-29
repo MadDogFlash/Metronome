86223de70cba067db2e9d01c65015cabd2cdf78f
Razi D <racieldiz@gmail.com>
2020-07-28 17:55:34 -0700

    Updates

---
+++ precache-manifest.fbd06bc5ce16f72d02bc3748ac12ef4f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08c955fa47f99c0e4601d34928093722",
+    "url": "/MadDogFlash/Metronome/index.html"
+  },
+  {
+    "revision": "04d027cc1217b23881e7",
+    "url": "/MadDogFlash/Metronome/static/css/main.26d66633.chunk.css"
+  },
+  {
+    "revision": "427f42762413a191f1bf",
+    "url": "/MadDogFlash/Metronome/static/js/2.e22a46a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MadDogFlash/Metronome/static/js/2.e22a46a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04d027cc1217b23881e7",
+    "url": "/MadDogFlash/Metronome/static/js/main.1627c833.chunk.js"
+  },
+  {
+    "revision": "dbcf3b4dbe69c2cdae9c",
+    "url": "/MadDogFlash/Metronome/static/js/runtime-main.4620a6b8.js"
+  },
+  {
+    "revision": "7a7930a26ffe78ed1c7375364bde24c2",
+    "url": "/MadDogFlash/Metronome/static/media/click1.7a7930a2.wav"
+  },
+  {
+    "revision": "fe0ded65f5f6b999883ee7aa4cf418c4",
+    "url": "/MadDogFlash/Metronome/static/media/click2.fe0ded65.wav"
+  }
+]);
